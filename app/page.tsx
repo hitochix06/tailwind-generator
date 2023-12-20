4d657f721835397498787b5bb6bdc7a9b38a74e8
hitochix06 <pachara.philibert@hotmail.fr>
2023-12-20 09:33:47 +0100

    Add form submission functionality to Home component

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,23 @@
 import { Sparkle, Sparkles } from "lucide-react";
 
 export default function Home() {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+
+    const prompt = formData.get("prompt") as string;
+
+    const result = await fetch("/api/tailwind", {
+      method: "POST",
+      body: JSON.stringify({ prompt }),
+    });
+
+    const json = await result.json();
+
+    console.log(json);
+  };
+
   return (
     <main className="h-full flex relative">
       <div className="absolute top-4 left-0 right-0 flex items-center justify-center">
@@ -11,7 +28,10 @@ export default function Home() {
       <div className="fixed bottom-4 left-0 right-0 flex items-center justify-center">
         <form className="p-4 bg-base-200 max-w-lg w-full">
           <fieldset className="flex gap-4 items-start">
-            <textarea className=" w-full textarea textarea-primary" />
+            <textarea
+              name="prompt"
+              className=" w-full textarea textarea-primary"
+            />
             <button className="btn btn-primary btn-sm " type="submit">
               <Sparkles size={20} />
             </button>
