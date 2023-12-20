725bb58f7d9fef83bc7764faf801da589f9db667
hitochix06 <pachara.philibert@hotmail.fr>
2023-12-20 14:05:19 +0100

    Refactor page.tsx to use timed state for HTML code

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,33 @@
 "use client";
 
-import { Sparkle, Sparkles } from "lucide-react";
-import { useState } from "react";
+import { Sparkles } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+
+const useTimedState = (state: unknown, delay: 2000) => {
+  const [timedState, setTimedState] = useState(state);
+
+  const lastUpdateRef = useRef(Date.now());
+
+  useEffect(() => {
+    if (Date.now() - lastUpdateRef.current > delay) {
+      setTimedState(state);
+      lastUpdateRef.current = Date.now();
+      return;
+    }
+
+    const timeout = setTimeout(() => {
+      setTimedState(state);
+      lastUpdateRef.current = Date.now();
+    }, delay - (Date.now() - lastUpdateRef.current));
+    return () => clearTimeout(timeout);
+  }, [state, delay]);
+
+  return timedState;
+};
 
 export default function Home() {
   const [htmlCode, setHtmlCode] = useState("");
+  const timedHtmlCode = useTimedState(htmlCode, 2000);
   const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -55,8 +78,9 @@ export default function Home() {
         </div>
       ) : null}
 
-      <pre>{htmlCode}</pre>
-      {htmlCode ? (
+      {/* <pre>{htmlCode}</pre> // for debug */}
+
+      {timedHtmlCode ? (
         <iframe
           className="w-full h-full"
           // add tailwind cdn <script src="https://cdn.tailwindcss.com"></script> and htmlcode
@@ -66,20 +90,18 @@ export default function Home() {
             <meta charset="UTF-8" />
             <title>Document</title>
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <link
-              rel="stylesheet"
-              href="https://cdn.tailwindcss.com"
+            <script src="https://cdn.tailwindcss.com"></script>
           </head>
           <body>
-            ${htmlCode}
+            ${timedHtmlCode}
           </body>
           </html>`}
         />
       ) : null}
 
-      <div className="fixed bottom-4 left-0 right-0 flex items-center justify-center">
+      <div className="fixed bottom-4 left-0 right-0 flex  items-center justify-center">
         <form
-          className="p-4 bg-base-200 max-w-lg w-full "
+          className="p-4 bg-base-200 max-w-lg w-full rounded-lg shadow-xl "
           onSubmit={handleSubmit}
         >
           <fieldset className="flex gap-4 items-start">
